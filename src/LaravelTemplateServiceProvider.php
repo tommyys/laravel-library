318d3e0429677d78c955c33634d519532fcc6bc7
Tommy Lee <tommy.lee@axstarzy.com>
2019-04-25 13:41:03 +0800

    testing

--- src/LaravelTemplateServiceProvider.php
+++ src/LaravelTemplateServiceProvider.php
@@ -5,9 +5,6 @@ namespace Axstarzy\LaravelTemplate;
 use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 
-use Collective\Html\HtmlServiceProvider;
-use Intervention\Image\ImageServiceProvider;
-
 class LaravelTemplateServiceProvider extends ServiceProvider
 {
     /**
@@ -17,16 +14,14 @@ class LaravelTemplateServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $loader = AliasLoader::getInstance();
+        // // laravelcollective/html
+        // $this->app->register(Collective\Html\HtmlServiceProvider::class);
+        // $loader->alias('Form', '\Collective\Html\FormFacade');
+        // $loader->alias('Html', '\Collective\Html\HtmlFacade');
 
-        // laravelcollective/html
-        $this->app->register(Collective\Html\HtmlServiceProvider::class);
-        $loader->alias('Form', '\Collective\Html\FormFacade');
-        $loader->alias('Html', '\Collective\Html\HtmlFacade');
-
-        // intervention/image
-        $this->app->register(Intervention\Image\ImageServiceProvider::class);
-        $loader->alias('Image', '\Intervention\Image\Facades\Image');
+        // // intervention/image
+        // $this->app->register(Intervention\Image\ImageServiceProvider::class);
+        // $loader->alias('Image', '\Intervention\Image\Facades\Image');
 
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
     }

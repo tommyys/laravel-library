b2a8c7ba09f76e235984569b97dfca58cb07d7c8
Tommy Lee <tommy.lee@axstarzy.com>
2019-05-06 11:47:57 +0800

    updated error log migration

--- src/database/migrations/2019_04_25_080813_create_error_log_table.php
+++ src/database/migrations/2019_04_25_080813_create_error_log_table.php
@@ -16,6 +16,7 @@ class CreateErrorLogTable extends Migration
         Schema::create('error_log', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
+            $table->string('request');
             $table->string('error');
             $table->timestamps();
         });

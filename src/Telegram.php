ec82f99132fe12bed711ce6209b4fec82fd7daff
Tommy Lee <tommy.lee@axstarzy.com>
2019-05-29 15:08:55 +0800

    added telegram helper

---
+++ src/Telegram.php
@@ -0,0 +1,66 @@
+<?php
+
+// use this link to get your chat id
+// https://api.telegram.org/bot677341098:AAEX6-kv_4gC8y-cvuFcwZKA6mbzjBXj3eU/getUpdates
+
+namespace Axstarzy\LaravelTemplate;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+use Exception;
+use Log;
+
+class Telegram {
+
+    public static function notification($message, $chatID){
+        if(!$chatID){
+            $chatID = env('TELEGRAM_NOTI_GROUP');
+        }
+        // if env not production dont send
+        if(!Setting::env('production')){
+            return false;
+        };
+
+        $params = [
+            'chat_id' => $chatID,
+            'text' => $message
+        ];
+        
+        return self::httpGetRequest('sendMessage', $params);
+    }
+
+    public static function imgNotification($image_path, $chatID = null){
+        if(!$chatID){
+            $chatID = env('TELEGRAM_NOTI_GROUP');
+        }
+        // if env not production dont send
+        if(!Setting::env('production')){
+            return false;
+        };
+
+        $params = [
+            'chat_id' => $chatID,
+            'photo' => $image_path
+        ];
+        
+        return self::httpGetRequest('sendphoto', $params);
+    }
+
+    private static function httpGetRequest($route, $params){
+        $client = new Client(['verify' => false ]);
+        $params['parse_mode'] = 'Markdown';
+        $params['disable_web_page_preview'] = True;
+        $reqURL = "https://api.telegram.org/bot677341098:AAEX6-kv_4gC8y-cvuFcwZKA6mbzjBXj3eU/" . $route . "?".http_build_query($params);
+        try {
+          $res = $client->get($reqURL);
+        } catch(Exception $e) {
+            Log::info("Telegram sent failed");
+            Log::info($e->getMessage());
+            return false;
+        }
+        return true;
+    }
+   
+}
+
